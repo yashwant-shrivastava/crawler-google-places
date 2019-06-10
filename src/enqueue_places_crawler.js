ea5d1e9607b8633042a97c102df41a8eb3dd3653
drobnikj <drobnik.j@gmail.com>
2019-06-10 16:57:19 +0200

    Fixed enqueueing places

--- src/enqueue_places_crawler.js
+++ src/enqueue_places_crawler.js
@@ -4,6 +4,12 @@ const { sleep, log } = Apify.utils;
 const { DEFAULT_TIMEOUT, LISTING_PAGINATION_KEY } = require('./consts');
 const { waitForGoogleMapLoader } = require('./utils');
 
+const clickOnPlaceDetail = async (page, link) => {
+    await link.click();
+    await waitForGoogleMapLoader(page);
+    await sleep(1000);
+};
+
 const enqueueAllUrlsFromPagination = async (page, requestQueue, paginationFrom, maxPlacesPerCrawl) => {
     let results = await page.$$('.section-result');
     const resultsCount = results.length;
@@ -17,9 +23,12 @@ const enqueueAllUrlsFromPagination = async (page, requestQueue, paginationFrom, 
         }, { timeout: DEFAULT_TIMEOUT }, resultIndex);
         results = await page.$$('.section-result');
         const link = await results[resultIndex].$('h3');
-        await link.click();
-        await waitForGoogleMapLoader(page);
-        await page.waitForSelector('.section-back-to-list-button', { timeout: 20000 });
+        await clickOnPlaceDetail(page, link);
+        // If there is still list of places ,try to click again
+        if (await page.$('.section-result')) {
+            await clickOnPlaceDetail(page, link);
+        }
+        await page.waitForSelector('.section-back-to-list-button', { timeout: 30000 });
         // After redirection to detail page, save the URL to Request queue to process it later
         const url = page.url();
         // Parse unique key from url if it is possible
