7cf0a38b0841f764b045d98774828613a0c33d68
metalwarrior665 <lukaskrivka@gmail.com>
2020-10-28 00:11:59 +0100

    update backward compat and deprecations

--- src/backwards-compatible-input.js
+++ src/backwards-compatible-input.js
@@ -1,10 +1,19 @@
+const Apify = require('apify');
+
+const { log } = Apify.utils;
+
+// Small hack for backward compatibillity
+// Previously there was a checkbox includeImages and includeReviews. It had to be on.
+// maxImages and maxReviews 0 or empty scraped all
+// Right now, it works like you woudl expect, 0 or empty means no images, for all images just set 99999
+// If includeReviews/includeImages is not present, we process regularly
 module.exports = (input) => {
-    // Small hack for backward compatibillity
-    // Previously there was a checkbox includeImages and includeReviews. It had to be on.
-    // maxImages and maxReviews 0 or empty scraped all
-    // Right now, it works like you woudl expect, 0 or empty means no images, for all images just set 99999
-    // If includeReviews/includeImages is not present, we process regularly
+    // Deprecated on 2020-07
+    if (input.includeReviews !== undefined || input.includeImages !== undefined) {
+        log.warning('DEPRECATION: includeReviews and includeImages input fields have been deprecated and will be removed soon! Use maxImage and maxReviews instead');
+    }
     if (input.includeReviews === true && !input.maxReviews) {
+
         input.maxReviews = 999999;
     }
 
@@ -18,5 +27,11 @@ module.exports = (input) => {
 
     if (input.includeImages === false) {
         input.maxImages = 0;
+    }
+
+    // Deprecated on 2020-10-27
+    if (input.forceEng) {
+        log.warning('DEPRECATION: forceEng input field have been deprecated and will be removed soon! Use language instead');
+        input.language = 'en';
     }
 };
