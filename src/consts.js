cfa2071fca0f88b5d98617b4aecadb0b5aa7c947
Paulo Cesar <email@pocesar.e4ward.com>
2021-05-26 17:03:19 -0300

    hotfix for detail title

--- src/consts.js
+++ src/consts.js
@@ -4,5 +4,5 @@ exports.DEFAULT_TIMEOUT = 60 * 1000; // 60 sec
 exports.LISTING_PAGINATION_KEY = 'lisState';
 exports.MAX_PAGE_RETRIES = 6;
 
-exports.PLACE_TITLE_SEL = 'h1.section-hero-header-title-title';
+exports.PLACE_TITLE_SEL = 'h1[class*="header-title-title"]';
 exports.BACK_BUTTON_SEL = 'button[jsaction*=back], button[aria-label="Back"]';
