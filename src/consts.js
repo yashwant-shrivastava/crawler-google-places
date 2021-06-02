60b59785d0ceb50a430c9bf9b5ec7686eb2dbd92
Jochen Fromm <j.fromm@reply.de>
2021-06-02 16:42:10 +0200

    Fix outdated selector

--- src/consts.js
+++ src/consts.js
@@ -6,3 +6,6 @@ exports.MAX_PAGE_RETRIES = 6;
 
 exports.PLACE_TITLE_SEL = 'h1[class*="header-title-title"]';
 exports.BACK_BUTTON_SEL = 'button[jsaction*=back], button[aria-label="Back"]';
+exports.NEXT_BUTTON_SELECTOR = '[jsaction="pane.paginationSection.nextPage"]';
+
+exports.NO_RESULT_XPATH = '//div[contains(text(), "No results found")]';
